1e57e782cf04525aba069284d825bf09677aaf18
Tiffany Zhang <tiffz13@uci.edu>
2025-04-22 16:00:37 -0700

    Inital commit

---
+++ src/pages/JoinUs.jsx
@@ -0,0 +1,9 @@
+import TimeLine from '../components/TimeLine';
+
+export default function JoinUs() {
+    return (
+        <main className="pt-20">
+            <TimeLine />
+        </main>
+    );
+}
