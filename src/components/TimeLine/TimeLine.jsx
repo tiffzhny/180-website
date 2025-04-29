edc01e72313939d7d31127253a2ca0255f46b1f8
Tiffany Zhang <tiffz13@uci.edu>
2025-04-28 20:14:02 -0700

    updates

--- src/components/TimeLine/TimeLine.jsx
+++ src/components/TimeLine/TimeLine.jsx
@@ -1,71 +1,114 @@
-import { FaInfoCircle, FaArrowUp, FaHandshake } from 'react-icons/fa';
+import { useEffect } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import { FaInfoCircle } from 'react-icons/fa';
 import React from 'react';
-import { useEffect } from 'react';
-
-
-const timelineEvents = [
-  {
-    date: '12/16',
-    title: 'Applications Open',
-    description: 'Apply at https://tinyurl.com/180dcwinter2025',
-    icon: <FaInfoCircle />,
-  },
-  {
-    date: '1/8',
-    title: 'Virtual Info Session (time TBD)',
-    description: 'Zoom Info: Meeting ID: 444 305 8677 Passcode: 180dcuci',
-    icon: <FaInfoCircle />,
-  },
-  {
-    date: '1/14',
-    title: 'In-Person Info Session (time TBD)',
-    description: 'Room Location TBD',
-    icon: <FaInfoCircle />,
-  },
-  {
-    date: '1/15',
-    title: 'Applications Due @ 11:59 PM PDT',
-    description: '',
-    icon: <FaArrowUp />,
-  },
-  {
-    date: '1/18 & 1/19',
-    title: 'Interview Rounds 1 & 2 (Invite Only)',
-    description: '',
-    icon: <FaHandshake />,
-  },
-];
 
 export default function Timeline() {
-    useEffect(() => {
-        AOS.init({
-            duration: 700,
-            once: true,
-        });
-    }, []);
-    return (
-        <section className="bg-white py-12 px-6 max-w-3xl mx-auto">
-            <h2 className="text-3xl font-bold text-center mb-10">Recruitment Timeline</h2>
-            <div className="relative border-l-5 rounded border-green-300 pl-6">
-                {timelineEvents.map((event, index) => (
-                    <div key={index} className="mb-10 relative">
-                        <div className="absolute -left-6 top-1 text-green-600 bg-white rounded-full p-1 shadow">
-                            <div className="text-lg">{event.icon}</div>
-                        </div>
-                        <div key = {index} data-aos ="fade-up" data-aos-delay={index * 100}
-                            className="bg-gray-50 rounded-md p-6 shadow">
-                            <h3 className="text-lg font-semibold">
-                                {event.date} : {event.title}
-                            </h3>
-                            {event.description && (
-                                <p className="text-gray-700 mt-1 text-sm">{event.description}</p>
-                            )}
-                        </div>
-                    </div>
-                ))}
-            </div>
-        </section>
-    );
+  useEffect(() => {
+    AOS.init({ duration: 700, once: true });
+  }, []);
+
+  return (
+    <ul className="timeline timeline-vertical">
+  <li>
+    <div className="timeline-start">1984</div>
+    <div className="timeline-middle">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+        className="h-5 w-5"
+      >
+        <path
+          fillRule="evenodd"
+          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+          clipRule="evenodd"
+        />
+      </svg>
+    </div>
+    <div className="timeline-end timeline-box">First Macintosh computer</div>
+    <hr />
+  </li>
+  <li>
+    <hr />
+    <div className="timeline-start">1998</div>
+    <div className="timeline-middle">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+        className="h-5 w-5"
+      >
+        <path
+          fillRule="evenodd"
+          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+          clipRule="evenodd"
+        />
+      </svg>
+    </div>
+    <div className="timeline-end timeline-box">iMac</div>
+    <hr />
+  </li>
+  <li>
+    <hr />
+    <div className="timeline-start">2001</div>
+    <div className="timeline-middle">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+        className="h-5 w-5"
+      >
+        <path
+          fillRule="evenodd"
+          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+          clipRule="evenodd"
+        />
+      </svg>
+    </div>
+    <div className="timeline-end timeline-box">iPod</div>
+    <hr />
+  </li>
+  <li>
+    <hr />
+    <div className="timeline-start">2007</div>
+    <div className="timeline-middle">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+        className="h-5 w-5"
+      >
+        <path
+          fillRule="evenodd"
+          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+          clipRule="evenodd"
+        />
+      </svg>
+    </div>
+    <div className="timeline-end timeline-box">iPhone</div>
+    <hr />
+  </li>
+  <li>
+    <hr />
+    <div className="timeline-start">2015</div>
+    <div className="timeline-middle">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+        className="h-5 w-5"
+      >
+        <path
+          fillRule="evenodd"
+          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+          clipRule="evenodd"
+        />
+      </svg>
+    </div>
+    <div className="timeline-end timeline-box">Apple Watch</div>
+  </li>
+</ul>
+  )
 }
--- src/pages/JoinUs.jsx
+++ src/pages/JoinUs.jsx
@@ -1,4 +1,5 @@
 import TimeLine from '../components/TimeLine/TimeLine';
+import React from 'react';
 
 export default function JoinUs() {
     return (
